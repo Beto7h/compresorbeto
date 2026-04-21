3049ad6055fcbafe9e377281701db0c50a75ca7c
Beto7h <gruperolyricoficial@gmail.com>
2026-04-21 12:47:48 -0600

    run_bot.sh

--- run_bot.sh
+++ run_bot.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Iniciar Aria2 en segundo plano con el RPC activo
+# --- PASO A: Iniciar el motor de Aria2 ---
+# --enable-rpc: Activa la comunicación con el bot
+# --rpc-listen-all: Permite que escuche peticiones
+# --daemon=true: Lo lanza en segundo plano
+echo "🌐 Iniciando servidor Aria2 RPC..."
 aria2c --enable-rpc --rpc-listen-all --daemon=true
 
-# Lanzar el bot de Python
+# --- PASO B: Pequeña espera ---
+# Damos 2 segundos para que Aria2 termine de levantar el puerto
+sleep 2
+
+# --- PASO C: Iniciar el Bot ---
+echo "🚀 Lanzando CompresorBeto..."
 python3 main.py
