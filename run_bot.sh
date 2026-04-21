84b8efcd2cb73c809a475e8d5ef6b5317bbcb7ac
Beto7h <gruperolyricoficial@gmail.com>
2026-04-21 13:46:16 -0600

    run_bot.sh

--- run_bot.sh
+++ run_bot.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# --- PASO A: Iniciar el motor de Aria2 ---
-# --enable-rpc: Activa la comunicación con el bot
-# --rpc-listen-all: Permite que escuche peticiones
-# --daemon=true: Lo lanza en segundo plano
 echo "🌐 Iniciando servidor Aria2 RPC..."
-aria2c --enable-rpc --rpc-listen-all --daemon=true
+# Añadimos --dir=/app para sincronizar rutas con el bot
+aria2c --enable-rpc --rpc-listen-all --dir=/app --daemon=true
 
-# --- PASO B: Pequeña espera ---
-# Damos 2 segundos para que Aria2 termine de levantar el puerto
 sleep 2
 
-# --- PASO C: Iniciar el Bot ---
 echo "🚀 Lanzando CompresorBeto..."
 python3 main.py
